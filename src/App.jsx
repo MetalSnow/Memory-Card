2e522d014ba7b93bd743de26d8117f387c021275
MetalSnow <aymanetrend@gmail.com>
2024-09-13 19:41:30 +0800

    feat: Render all cards with names
    -Fetched object of images data and stored it.
    -Lifted the state up to make Card component reusable.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
 import Card from './components/Card';
 import './styles/App.css';
+import { createClient } from 'pexels';
+import { useEffect, useState } from 'react';
 
-function App() {
+const key = import.meta.env.VITE_API_KEY;
+const client = createClient(key);
+
+function MemoryCard() {
+  const [photosObj, setPhotosObj] = useState([]);
+
+  const query = 'animal';
+
+  useEffect(() => {
+    let isMount = true;
+
+    client.photos.search({ query }).then((response) => {
+      if (isMount) {
+        console.log(response.photos);
+        setPhotosObj(response.photos);
+      }
+    });
+
+    return () => (isMount = false);
+  }, []);
+
   return (
     <>
-      <Card />
+      <header>
+        <div>
+          <h1>Animals Memory Game</h1>
+          <p>
+            Get points by clicking on an image but don&#39;t click on any more
+            than once!
+          </p>
+        </div>
+      </header>
+      <div className="cards">
+        {photosObj.map((photo) => (
+          <div className="card" key={photo.id}>
+            <Card name={photo.alt} url={photo.src.medium} />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
 
-export default App;
+export default MemoryCard;
